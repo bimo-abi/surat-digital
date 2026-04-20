ee1adaf2236dad0ff4481b8ef8a6f8ec6b4ef3cf
AdiPtrrr <tonasputra17@gmail.com>
2026-04-20 09:30:04 +0700

    fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masuk ke Akun</title>
-    <link rel="stylesheet" href="surat-digital/resources/css/login.css">
+    <link rel="stylesheet" href="/surat-digital/resources/css/login.css">
 </head>
 <body>
 
